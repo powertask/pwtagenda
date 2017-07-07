7ee66f48c99b9f326aad43cf7164bc723f686ef7
Marcelo Reichert <marcelo@powertask.com.br>
2017-07-07 18:42:30 -0300

    asd

--- public/assets/application-93f1fc0cc5de233f83b3070837cd802fe425d7bf10220c7e534b1170ef22a740.js
+++ public/assets/application-93f1fc0cc5de233f83b3070837cd802fe425d7bf10220c7e534b1170ef22a740.js
@@ -57755,6 +57755,92 @@ $(function() {
     firstDay: 1
   });
 });
+
+
+$(function() {
+  $('#attended_event').click(function (event) {
+    var targetUrl = $(this).attr("href");
+    $.confirm({
+        title: 'Compareceu?',
+        content: 'Marcar comparecimemto na hora marcada',
+      confirmButton: 'Sim',
+      cancelButton: 'Cancelar',
+      confirmButtonClass: 'btn-danger',
+      theme: 'supervan',
+        confirm: function(){
+            window.location.href = targetUrl;
+        },
+        cancel: function(){
+
+        }
+    });
+    return false;
+  });
+});
+
+$(function() {
+  $('#not_attended_event').click(function (event) {
+    var targetUrl = $(this).attr("href");
+    $.confirm({
+        title: 'Faltou?',
+        content: 'Deseja marcar a falta na hora marcada?',
+      confirmButton: 'Sim',
+      cancelButton: 'Cancelar',
+      confirmButtonClass: 'btn-danger',
+      theme: 'supervan',
+        confirm: function(){
+            window.location.href = targetUrl;
+        },
+        cancel: function(){
+
+        }
+    });
+    return false;
+  });
+});
+
+$(function() {
+  $('#get_unmarked_event').click(function (event) {
+    var targetUrl = $(this).attr("href");
+    $.confirm({
+        title: 'Desmarcar?',
+        content: 'Deseja desmarcar a hora marcada?',
+      confirmButton: 'Sim',
+      cancelButton: 'Cancelar',
+      confirmButtonClass: 'btn-danger',
+      theme: 'supervan',
+        confirm: function(){
+            window.location.href = targetUrl;
+        },
+        cancel: function(){
+
+        }
+    });
+    return false;
+  });
+});
+
+$(function() {
+  $('#delete_agenda').click(function (event) {
+    var targetUrl = $(this).attr("href");
+    $.confirm({
+        title: 'Deletar agenda?',
+        content: 'Deseja deletar a agenda criada?',
+      confirmButton: 'Sim',
+      cancelButton: 'Cancelar',
+      confirmButtonClass: 'btn-danger',
+      theme: 'supervan',
+        confirm: function(){
+            window.location.href = targetUrl;
+        },
+        cancel: function(){
+
+        }
+    });
+    return false;
+  });
+});
+
 /*
 Turbolinks 5.0.3
 Copyright © 2017 Basecamp, LLC
